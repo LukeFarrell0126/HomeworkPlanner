6222c6bf0666772bbf14bd11d26f2b4719d7870b
farrelll241 <lukefarrell153@gmail.com>
2023-04-21 10:37:16 -0400

    Main Fragment work

---
+++ app/src/main/java/com/example/homeworkplanner/ActionFragment.kt
@@ -0,0 +1,23 @@
+package com.example.homeworkplanner
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class ActionFragment : Fragment() {
+    lateinit var dbRef : DatabaseReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dbRef = Firebase.database.reference //in view model or every fragment
+
+        return inflater.inflate(R.layout.fragment_action, container, false)
+    }
+}
