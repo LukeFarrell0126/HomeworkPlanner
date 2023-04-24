e7eaf1ba05af72aa8098b6e16fc712cf192fa4ee
farrelll241 <lukefarrell153@gmail.com>
2023-04-24 10:37:46 -0400

    Action Fragment and main fragment work

--- app/src/main/java/com/example/homeworkplanner/MainFragment.kt
+++ app/src/main/java/com/example/homeworkplanner/MainFragment.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.homeworkplanner.databinding.FragmentMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    lateinit var dbRef : DatabaseReference
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        dbRef = Firebase.database.reference //in view model or every fragment
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val rootView = binding.root
+
         return rootView
     }
 }
