8a97e375c5d88594e993b83e889dc90b66b02317
farrelll241 <lukefarrell153@gmail.com>
2023-04-25 10:38:30 -0400

    action fragment

--- app/src/main/java/com/example/homeworkplanner/ActionFragment.kt
+++ app/src/main/java/com/example/homeworkplanner/ActionFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.homeworkplanner.databinding.FragmentActionBinding
 import com.example.homeworkplanner.databinding.FragmentMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -12,16 +13,18 @@ import com.google.firebase.ktx.Firebase
 
 class ActionFragment : Fragment() {
     lateinit var dbRef : DatabaseReference
-    private var _binding: FragmentMainBinding? = null
+    private var _binding: FragmentActionBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         dbRef = Firebase.database.reference //in view model or every fragment
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        _binding = FragmentActionBinding.inflate(inflater, container, false)
         val rootView = binding.root
+        binding.addButton.setOnClickListener{
 
+        }
         return rootView
     }
 }
--- app/src/main/java/com/example/homeworkplanner/MainFragment.kt
+++ app/src/main/java/com/example/homeworkplanner/MainFragment.kt
@@ -1,10 +1,12 @@
 package com.example.homeworkplanner
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.homeworkplanner.databinding.FragmentMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -23,6 +25,10 @@ class MainFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val rootView = binding.root
 
+        binding.beginPlanningButton.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToActionFragment()
+            rootView.findNavController().navigate(action)
+        }
         return rootView
     }
 }
